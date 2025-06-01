755b547853315100e422328a2e68375a24a25e6a
ZyanAlkanza <zyan.mujaddid@gmail.com>
2025-06-01 11:18:54 +0700

    feat: kartu di transaksi

---
+++ src/components/transaksi/Kartu.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="">
+        <div class="row q-col-gutter-sm">
+            <div class="col">
+                <div class="row secondary-color q-pa-md" style="border-radius: 6px;">
+                    <div class="row justify-center items-center blue-color" style="font-size: 36px; border-radius: 8px;">
+                        <q-icon name="attach_money" color="primary" class="q-pa-md"/>
+                    </div>
+                    <div class="q-px-md text-white">
+                        <div style="font-size: 14px; font-weight: 300;">
+                            Total Saldo
+                        </div>
+                        <div style="font-size: 24px; font-weight: 500;">
+                            Rp. 1.800.000
+                        </div>
+                        <div class="row text-green" style="font-size: 12px; font-weight: 300;">
+                            <q-icon name="north_east" style="font-size: 16px;"/>
+                            <div class="q-ml-xs">
+                                4% dari bulan lalu
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row secondary-color q-pa-md" style="border-radius: 6px;">
+                    <div class="row justify-center items-center green-color" style="font-size: 36px; border-radius: 8px;">
+                        <q-icon name="trending_up" color="green" class="q-pa-md"/>
+                    </div>
+                    <div class="q-px-md text-white">
+                        <div style="font-size: 14px; font-weight: 300;">
+                            Pemasukan
+                        </div>
+                        <div style="font-size: 24px; font-weight: 500;">
+                            Rp. 3.000.000
+                        </div>
+                        <div class="row text-green" style="font-size: 12px; font-weight: 300;">
+                            <q-icon name="north_east" style="font-size: 16px;"/>
+                            <div class="q-ml-xs">
+                                30% dari bulan lalu
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row secondary-color q-pa-md" style="border-radius: 6px;">
+                    <div class="row justify-center items-center red-color" style="font-size: 36px; border-radius: 8px;">
+                        <q-icon name="trending_down" color="red" class="q-pa-md"/>
+                    </div>
+                    <div class="q-px-md text-white">
+                        <div style="font-size: 14px; font-weight: 300;">
+                            Pengeluaran
+                        </div>
+                        <div style="font-size: 24px; font-weight: 500;">
+                            Rp. 2.000.000
+                        </div>
+                        <div class="row text-red" style="font-size: 12px; font-weight: 300;">
+                            <q-icon name="north_east" style="font-size: 16px;"/>
+                            <div class="q-ml-xs">
+                                20% dari bulan lalu
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row secondary-color q-pa-md" style="border-radius: 6px;">
+                    <div class="row justify-center items-center yellow-color" style="font-size: 36px; border-radius: 8px;">
+                        <q-icon name="attach_money" color="yellow" class="q-pa-md"/>
+                    </div>
+                    <div class="q-px-md text-white">
+                        <div style="font-size: 14px; font-weight: 300;">
+                            Selisih
+                        </div>
+                        <div style="font-size: 24px; font-weight: 500;">
+                            Rp. 1.000.000
+                        </div>
+                        <div class="row text-green" style="font-size: 12px; font-weight: 300;">
+                            <q-icon name="north_east" style="font-size: 16px;"/>
+                            <div class="q-ml-xs">
+                                Surplus
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/Transaksi.vue
+++ src/pages/Transaksi.vue
@@ -1,5 +1,86 @@
 <template>
-    <div>
-        test
+    <div class="row" style="overflow: hidden">
+        <div class="col-12">
+            <kartu/>
+        </div>
+        <div class="col-6">
+            <div class="q-pt-sm q-pr-xs">
+                <div class="scroll-container">
+                    
+                    <div class="row q-mb-sm secondary-color text-white" style="border-radius: 6px;" v-for="n in 15" :key="n">
+                        <div class="col-12">
+                            <div class="row q-py-sm q-px-md justify-between items-center">
+                                <div class="row">
+                                    <div style="width: 44px; font-size: 24px; font-weight: 500; text-align: center;">21</div>
+                                    <div class="row justify-center" style="flex-direction: column;">
+                                        <div style="line-height: 14px;">Jum'at</div>
+                                        <div class="font-secondary-color" style="font-size: 12px; line-height: 14px;">Maret 2025</div>
+                                    </div>
+                                </div>
+                                <div style="font-weight: 500;">Rp. 200.000</div>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="row q-py-sm q-px-md justify-between items-center">
+                                <div class="row">
+                                    <div style="text-align: center; width: 44px;">
+                                        <q-icon name="vertical_align_bottom" size="md"/>
+                                    </div>
+                                    <div class="row justify-center" style="flex-direction: column;">
+                                        <div style="line-height: 14px;">Transfer Masuk</div>
+                                        <div class="q-mt-xs font-secondary-color" style="font-size: 12px; line-height: 14px;">Bank Mandiri</div>
+                                    </div>
+                                </div>
+                                <div class="text-green" style="font-weight: 500;">Rp. 200.000</div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="col-12">
+            <div class="row q-col-gutter-sm">
+                <div class="col-4 q-mt-sm">
+                    <pemasukaanTerakhir/>
+                </div>
+                <div class="col-4 q-mt-sm">
+                    <pengeluaranTerakhir/>
+                </div>
+                <div class="col-4 q-mt-sm">
+                    <pengeluaranBerdasarkanKategori/>
+                </div>
+            </div>
+        </div> -->
     </div>
 </template>
+
+<script>
+import kartu from '../components/transaksi/Kartu.vue' 
+
+export default({
+    components:{
+        kartu,
+    },
+    data() {
+        return {
+            
+        }
+    },
+})
+</script>
+
+<style scoped>
+.scroll-container {
+    height: 600px;
+    overflow-y: auto;
+    -ms-overflow-style: none; /* for Internet Explorer, Edge */
+    scrollbar-width: none; /* for Firefox */
+}
+
+.scroll-container::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+}
+</style>
