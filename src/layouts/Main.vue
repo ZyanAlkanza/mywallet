8568ebd6a17953742b24a2458cb94e0480947c15
ZyanAlkanza <zyan.mujaddid@gmail.com>
2025-06-06 19:17:17 +0700

    feat: tambah transaksi di transaksi

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -15,13 +15,14 @@
             <div class="q-mt-xs" style="font-size: 12px; font-weight: 300; line-height: 14px;">{{ tanggalHariIni }}</div>
         </q-toolbar-title>
         <q-section class="row">
-          <q-btn dense icon="add" color="primary" style="width: 44px;">
+          <q-btn dense icon="add" color="primary" style="width: 44px;" @click="dialogTambahTransaksi">
             <q-tooltip class="bg-primary">
               Tambah Transaksi
             </q-tooltip>
           </q-btn>
 
           <q-select 
+            dark
             dense
             borderless
             color="primary"
@@ -42,7 +43,7 @@
             <q-menu>
               <div class="row no-wrap q-pa-md">
                 <div class="column">
-                  <q-select 
+                  <q-select
                     dense
                     borderless
                     v-model="bulan" 
@@ -143,6 +144,89 @@
           </q-list>
         </q-scroll-area>
       </q-drawer>
+
+      <q-dialog v-model="modalTambahTransaksi" :backdrop-filter="'saturate(50%)'">
+        <q-card class="primary-color text-white" style="width: 700px;">
+          <q-card-section class="row items-center q-pb-none">
+            <div class="text-h6">Tambah Transaksi</div>
+            <q-space />
+            <q-btn icon="close" flat round dense v-close-popup />
+          </q-card-section>
+
+          <q-card-section>
+            <div>
+              <span>Nominal</span>
+              <q-input
+                style="font-size: 32px;"
+                dark
+                dense
+                borderless
+                input-class="text-right"
+                mask="###.###.###.###"
+                maxlength="15"
+                reverse-fill-mask
+                v-model="nominal" 
+                prefix="Rp."
+              />
+            </div>
+            
+            <div class="row q-mt-sm q-col-gutter-md">
+              <div class="col-4">
+                <span>Dompet</span>
+                <q-select 
+                  dark
+                  dense
+                  borderless
+                  v-model="dompet" 
+                  :options="dompet_opt" 
+                />
+              </div>
+              <div class="col-4">
+                <span>Kategori</span>
+                <q-select 
+                  dark
+                  dense
+                  borderless
+                  v-model="kategori" 
+                  :options="kategori_opt" 
+                />
+              </div>
+              <div class="col-4">
+                <span>Tanggal</span>
+                <q-input
+                  dark
+                  dense
+                  borderless
+                  v-model="tanggalTransaksi"  
+                  type="date" 
+                />
+              </div>
+            </div>
+
+            <div class="q-mt-sm" style="width: 100%">
+              <span>Deskripsi</span>
+              <q-input
+                dark
+                dense
+                borderless
+                placeholder="Deskripsi transaksi"
+                v-model="deskripsi"
+                type="textarea"
+              />
+            </div>
+
+            <div class="row q-mt-md justify-end">
+              <q-btn
+                dense
+                icon="add"
+                color="primary"
+                label="Tambah Transaksi"
+                @click="tambahTransaksi"
+              />
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
       
       <q-page-container>
         <router-view />
@@ -151,8 +235,7 @@
 </template>
 
 <script>
-  import { tiUppercase } from '@quasar/extras/themify';
-import beranda from '../pages/Beranda.vue'
+  import beranda from '../pages/Beranda.vue'
   import dayjs from 'dayjs';
   import 'dayjs/locale/id'
 
@@ -163,15 +246,20 @@ import beranda from '../pages/Beranda.vue'
     data() {
       return {
         drawer: false,
-        tanggalHariIni: '',
-        dompet:'',
-        dompet_opt:['Uang Jajan', 'Uang Tabungan', 'Bank Mandiri', 'Bank BSI'],
-        tanggalTransaksi: '',
+        modalTambahTransaksi: false,
 
+        tanggalHariIni: '',
+        dompet: '',
+        dompet_opt:['Uang Jajan', 'Uang Tabungan', 'Bank Mandiri', 'Bank BSI'],
+        kategori:'',
+        kategori_opt:['Uang Jajan', 'Uang Tabungan', 'Bank Mandiri', 'Bank BSI'],
+        tanggalTransaksi: '',
         bulan: '',
         bulan_opt: '',
         tahun: '',
-        tahun_opt: ''
+        tahun_opt: '',
+        nominal: '0',
+        deskripsi: ''
       }
     },
     mounted() {
@@ -181,6 +269,12 @@ import beranda from '../pages/Beranda.vue'
       tanggal() {
         dayjs.locale('id')
         this.tanggalHariIni = dayjs().format('dddd, DD MMMM YYYY')
+      },
+      dialogTambahTransaksi() {
+        this.modalTambahTransaksi = true;
+      },
+      tambahTransaksi() {
+        
       }
     },
   })
